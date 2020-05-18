3f520ae16076f4749cc056c231665ecf88b6cf9d
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-18 21:30:02 +0200

    updated to RWD

--- src/components/LogIn/Circle/Circle.js
+++ src/components/LogIn/Circle/Circle.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Lock from '../../../assets/Union.svg'
+import Lock from '../../../assets/Union.svg';
+import Lock1 from '../../../assets/Union1.svg'
+
+const media = window.innerWidth;
+// dodać event nasłuchujący na zmianę
 
 function Circle() {
     return (
         <div className="Circle">
-            <img src={Lock} alt='lock' className='lock' />
+            <img src={media >= '1280' ? Lock1 : Lock} alt='lock' />
         </div>
     );
 }
