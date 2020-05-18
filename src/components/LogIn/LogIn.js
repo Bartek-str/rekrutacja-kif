d296ae08b62681ac6d3efdf5f6f759bcb49bab28
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-18 16:21:43 +0200

    maintext done

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -2,6 +2,7 @@ import React from "react";
 import Circle from "./Circle";
 import Exit from "./Exit";
 import Header from "./Header";
+import MainText from "./MainText";
 
 function LogIn() {
     return (
@@ -9,6 +10,7 @@ function LogIn() {
             <Circle />
             <Exit />
             <Header />
+            <MainText />
         </div>
     );
 }
---
+++ src/components/LogIn/MainText/MainText.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainText() {
+    return (
+        <div className='MainText'>
+            <span className='text'>Na podany numer telefonu wysłaliśmy SMS z kodem PIN</span>
+        </div>
+    );
+}
+
+export default MainText;
