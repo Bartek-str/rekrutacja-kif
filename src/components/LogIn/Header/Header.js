488c672929085e4ad3eea570bab650569f631a1a
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-18 16:14:49 +0200

    header done

---
+++ src/components/LogIn/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className='Header'>
+            <span className='text'>Podaj kod PIN</span>
+        </div>
+    );
+}
+
+export default Header;
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Circle from "./Circle";
 import Exit from "./Exit";
+import Header from "./Header";
 
 function LogIn() {
     return (
         <div className='LogIn'>
             <Circle />
             <Exit />
-
+            <Header />
         </div>
     );
 }
